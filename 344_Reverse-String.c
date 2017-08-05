6545061ca9760c35db0bf8aa899fc6b34297491f
yiyufxst <yiyufxst@alphathink.org>
2017-08-05 11:30:44 +0800

    modify the code format

--- 344_Reverse-String.c
+++ 344_Reverse-String.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-char* reverseString(char* s)
-{
+char* reverseString(char* s) {
     int i = 0, n = strlen(s) - 1;
-    while (i < n)
-    {
+    while (i < n) {
         char temp = s[i];
         s[i++] = s[n];
         s[n--] = temp;
     }
     
     return s;
+}
+
+int main() {
+	char s[] = "hello";
+	reverseString(s);
+	printf(s);
+	return 0;
 }
